d6286d2c7c1054042426959f150942ff8d06a67d
Elukoye <lukoyedith@gmail.com>
2019-12-03 23:05:56 +0300

    added a modulo_spec.rb

---
+++ modulo_spec.rb
@@ -0,0 +1,21 @@
+def division_operator(num)
+   num / 1000
+ end
+
+puts division_operator(1423)
+
+
+
+
+
+
+
+
+
+
+describe '#division_operator' do 
+  num = 1423
+  it 'returns the tens of places' do 
+    expect(division_operator(num)).to eq(1)
+  end
+end 
