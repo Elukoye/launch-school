bb1c21d90f7121b19596e0b9094b64b25f657bfb
Elukoye <lukoyedith@gmail.com>
2019-12-09 08:31:50 +0300

    added age.rb

---
+++ age.rb
@@ -0,0 +1,7 @@
+puts "What is your age?"
+age = gets.chomp.to_i + 10 
+age_one = age + 20 
+age_two = age_one + 20 
+puts " In 10yrs you will be #{age} yrs old"
+puts " In 10yrs you will be #{age_one} yrs old"
+puts " In 10yrs you will be #{age_two} yrs old"
